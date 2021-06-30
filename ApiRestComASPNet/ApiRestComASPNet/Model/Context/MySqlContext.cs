46e15d4ef100acfe4ac2ddc9129145975998bcbd
FelipeMatoso <73952306+FelipeModena@users.noreply.github.com>
2021-06-30 13:25:26 -0300

    add dbContext para a aplicação

---
+++ ApiRestComASPNet/ApiRestComASPNet/Model/Context/MySqlContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiRestComASPNet.Model.Context
+{
+    public class MySqlContext : DbContext
+    {
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base(options)
+        { }
+            public DbSet<Person> Person { get; set; }
+    }
+}
--- ApiRestComASPNet/ApiRestComASPNet/Startup.cs
+++ ApiRestComASPNet/ApiRestComASPNet/Startup.cs
@@ -1,7 +1,11 @@
+using ApiRestComASPNet.Model.Context;
+using ApiRestComASPNet.Services;
+using ApiRestComASPNet.Services.Implementations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,8 +29,16 @@ namespace ApiRestComASPNet
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            #region Metodo responsavel por descobrir os Controllers
             services.AddControllers();
+            #endregion
+            #region Regiao de conexao com banco de dados MySql
+            var MySqlConnectionString = Configuration.GetConnectionString("MySQLString");
+            services.AddDbContext<MySqlContext>(optionsAction => optionsAction.UseMySql(MySqlConnectionString, ServerVersion.AutoDetect(MySqlConnectionString)));
+            #endregion
+            #region Declaração de serviços para de Services
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
